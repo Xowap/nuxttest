3e22fbcfd294416c9603d4a87815285093d431f0
Rémy Sanchez <remy.sanchez@hyperthese.net>
2016-12-02 16:09:24 +0100

    PoC code

---
+++ components/HttpBin.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Here's what HTTP Bin says</h2>
+
+        <pre v-if="output">{{ output }}</pre>
+        <p v-if="!output">Loading...</p>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    import axios from 'axios';
+
+    export default {
+        data() {
+            return axios.get('https://httpbin.org/get').then((res) => {
+                return {
+                    output: res.data,
+                }
+            });
+        },
+    };
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Nuxt Async Test</h1>
+
+        <div v-if="output">
+            <h2>He's what HTTP Bin says (outside component)</h2>
+
+            <pre>{{ output }}</pre>
+        </div>
+
+        <http-bin></http-bin>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    import HttpBin from '~components/HttpBin.vue';
+    import axios from 'axios';
+
+    export default {
+        components: {
+            HttpBin,
+        },
+        data() {
+            return axios.get('https://httpbin.org/get').then((res) => {
+                return {
+                    output: res.data,
+                }
+            });
+        },
+    };
+</script>
